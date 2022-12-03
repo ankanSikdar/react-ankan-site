fb9a76a2d5d829487a985c71557fbf5c1a5d0b8b
Ankan Sikdar <ankansikdar@gmail.com>
2022-12-03 21:50:42 +0530

    Added my image

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,6 @@ export default function Home() {
         <meta name="description" content="Ankan's Portfolio Site" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className="dark:bg-gray-900">
         <div className="px-2  min-h-screen max-w-2xl mx-auto">
           <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
