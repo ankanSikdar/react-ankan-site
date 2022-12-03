81ea8aa564ce38f5020f27e073039b6518858977
Ankan Sikdar <ankansikdar@gmail.com>
2022-12-03 22:23:03 +0530

    Added text in about me

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -4,16 +4,28 @@ import ankan from "../public/assets/images/ankan.jpg";
 
 export default function AboutMe() {
   return (
-    <div className="pt-10 mx-auto flex justify-center">
-      <Image
-        src={ankan}
-        alt="Ankan's Image"
-        height={176}
-        width={176}
-        sizes="30vw"
-        priority
-        className="rounded-full filter dark:grayscale"
-      />
+    <div className="text-center text-black ">
+      <div className="pt-10 mx-auto flex justify-center">
+        <Image
+          src={ankan}
+          alt="Ankan's Image"
+          height={176}
+          width={176}
+          sizes="30vw"
+          priority
+          className="rounded-full filter dark:grayscale"
+        />
+      </div>
+      <h1 className="text-4xl mt-10 dark:text-white font-bold">
+        Hi I&apos;m Ankan 🤝🏽
+      </h1>
+      <p className="text-2xl mt-2 text-gray-800 dark:text-gray-200 font-semibold">
+        Learning and Building Flutter Apps
+      </p>
+      <div className="text-xl text-gray-600 dark:text-gray-400 font-medium mt-2">
+        <p>an App Developer from Kolkata, WB, India</p>
+        <p>I specialize in building Flutter Apps</p>
+      </div>
     </div>
   );
 }
