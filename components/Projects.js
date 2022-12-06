acbc0435d16f55b469448cb4fc3cad02de632e87
Ankan Sikdar <ankansikdar@gmail.com>
2022-12-06 21:20:23 +0530

    Updated header style

--- components/Projects.js
+++ components/Projects.js
@@ -1,7 +1,7 @@
 export default function Projects() {
   return (
     <div className="mt-20 mx-2 pb-20">
-      <h1 className="text-center text-5xl md:text-6xl dark:text-white mb-10">
+      <h1 className="text-center text-5xl md:text-6xl dark:text-white mb-10 font-extrabold uppercase tracking-widest leading-snug">
         Projects
       </h1>
     </div>
