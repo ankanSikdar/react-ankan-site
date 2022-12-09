97f03cbcd210fcf4f141f873143450156b0088d8
Ankan Sikdar <ankansikdar@gmail.com>
2022-12-09 18:47:11 +0530

    Added initial scale 1 meta tag

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
   return (
     <div className={isDarkMode ? "dark" : ""}>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Ankan Sikdar</title>
         <meta name="description" content="Ankan's Portfolio Site" />
         <link rel="icon" href="/favicon.ico" />
